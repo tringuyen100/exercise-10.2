c2d6a9748957e0f3f3e6e31e6399cc5994f35b9e
tringuyen100 <tri_nguyenhien@yahoo.com>
2015-03-26 23:04:36 -0400

    odd number for loop

---
+++ firstodd.java
@@ -0,0 +1,15 @@
+
+public class firstodd {
+	public static void main(String[] args) {
+		
+		
+		for(int i=0; i>=0 ; i--) {
+			System.out.println("The first 100 odd numbers");
+			for(int j=0; j<=99; j++) {
+				j= j+1;
+				System.out.println(j);
+			}
+			
+		}System.out.println("");
+	}
+}
