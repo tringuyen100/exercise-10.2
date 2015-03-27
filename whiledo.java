c0028788825c08a33f42aff63163ddd7f0e2c77d
tringuyen100 <tri_nguyenhien@yahoo.com>
2015-03-26 23:03:02 -0400

    whiledo even number
    Tri Nguyen

---
+++ whiledo.java
@@ -0,0 +1,18 @@
+
+public class whiledo {
+	public static void main(String[] args) {
+		int i = 0;
+		int limit = 100;
+		System.out.println("The first 100 even number are " + "\n");
+		do {
+		i= i+2;
+		System.out.println(i);}
+		while(i<limit);
+			
+		
+	}
+}
+
+
+
+
